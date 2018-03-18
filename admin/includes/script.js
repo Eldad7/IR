3922e2e18f2743ba6800b0e5a1ae5285c273c9c2
Eldad7 <eldad.corem@gmail.com>
2018-03-18 19:50:38 +0200

    Changes

--- admin/includes/script.js
+++ admin/includes/script.js
@@ -90,3 +90,30 @@ $(document).ready(function(){
 		});
 	});
 });
+
+function openFile(href,name){
+	$.ajax({
+		type:"POST",
+		url:'../getfile.php',
+		data:{href},
+		success:function(data){	
+			updateModal(name,data,true);
+		}
+	});
+}
+
+function updateModal(title,body,footer=false,redirect=false){
+    if (title!=null)
+        $('.modal-title').text(title);
+    if (body!=null)
+        $('.modal-body').html(body);
+    if (footer)
+        $(".modal-content").append('<div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div>');
+    if (!$('#myModal').hasClass('in'))
+        $('#myModal').modal('show');
+
+    $('#myModal').on('hidden.bs.modal', function (e) {
+        $('#myModal').modal('hide');
+        $(".modal-footer").remove();
+    });
+}
